95e2289b9a67efe8901b9ab0ed4c761fa088ba13
Sergiy <hondax11@ukr.net>
2023-03-30 21:07:50 +0200

    add fix model and add enums productType

--- BookStore.DataAccess/Models/Enums/ProductType.cs
+++ BookStore.DataAccess/Models/Enums/ProductType.cs
@@ -1,8 +1,16 @@
+<<<<<<< HEAD
 namespace BookStore.DataAccess.Models.Enums
+=======
+namespace BookStore.DataAccess.Models
+>>>>>>> e58c53c (add fix model and add enums productType)
 {
     public enum ProductType
     {
         Book,
+<<<<<<< HEAD
         Magazine
+=======
+        Magazin
+>>>>>>> e58c53c (add fix model and add enums productType)
     }
 }
