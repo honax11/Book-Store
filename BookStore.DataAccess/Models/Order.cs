1578b119879a225624f12b90c7b4f2dc2183e474
Sergiy <hondax11@ukr.net>
2023-04-10 22:20:06 +0200

    update model "virtual"

--- BookStore.DataAccess/Models/Order.cs
+++ BookStore.DataAccess/Models/Order.cs
@@ -10,7 +10,7 @@ namespace BookStore.DataAccess.Models
         public string Name { get; set; }
         public string PostIndex { get; set; }
         public string NumerPost { get; set; }
-        public List<Product> Products { get; set; }
+        public  virtual List<Product> Products { get; set; }
         public double TotalOrderPrice { get; set; }
         
     }
